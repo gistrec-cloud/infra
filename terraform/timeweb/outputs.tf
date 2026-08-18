2e9960a6f6b7b1e99281bef40db5d23bc004e098
Aleksandr Kovalko <gistrec@gmail.com>
2026-08-18 23:49:32 +0200

    timeweb: terraform fmt

--- terraform/timeweb/outputs.tf
+++ terraform/timeweb/outputs.tf
@@ -1,8 +1,8 @@
 output "russia_03" {
   description = "Timeweb Cloud server identity and public address."
   value = {
-    id        = twc_server.russia_03.id
-    status    = twc_server.russia_03.status
-    ipv4      = twc_floating_ip.russia_03.ip
+    id     = twc_server.russia_03.id
+    status = twc_server.russia_03.status
+    ipv4   = twc_floating_ip.russia_03.ip
   }
 }
